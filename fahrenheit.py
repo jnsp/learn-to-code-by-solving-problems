d2dd7f304466c2d0dc5232fc659179564b76f9a4
Jangsea Park <jangsea.park@gmail.com>
2025-02-22 21:26:42 +0900

    Add fahrenheit conversion function and corresponding tests (#5)

---
+++ fahrenheit.py
@@ -0,0 +1,10 @@
+# https://dmoj.ca/problem/wc17c1j2
+
+
+def fahrenheit(c):
+    return c * 9 / 5 + 32
+
+
+if __name__ == "__main__":
+    c = int(input())
+    print(fahrenheit(c))
---
+++ test_fahrenheit.py
@@ -0,0 +1,6 @@
+from fahrenheit import fahrenheit
+
+
+def test_celcius():
+    c = 20
+    assert fahrenheit(c) == 68
