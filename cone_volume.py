f27a0a0e55bad90cf77c3e919bb00071def82537
jnsp <jangsea.park@gmail.com>
2025-02-22 14:34:05 +0900

    Add interactive input for cone volume calculation

--- cone_volume.py
+++ cone_volume.py
@@ -5,3 +5,9 @@ import math
 
 def cone_volume(radius, height):
     return (1 / 3) * math.pi * radius**2 * height
+
+
+if __name__ == "__main__":
+    r = int(input())
+    h = int(input())
+    print(cone_volume(r, h))
