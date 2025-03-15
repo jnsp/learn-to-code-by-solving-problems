c2b24f9f24b241f355957c6b12fbb5fa53b57eb0
jnsp <jangsea.park@gmail.com>
2025-03-15 11:41:12 +0900

    Add second solution for dmoj-coci06c5p1

--- ch03/three_cups.py
+++ ch03/three_cups.py
@@ -1,7 +1,7 @@
 # https://dmoj.ca/problem/coci06c5p1
 
 
-def three_cups(swaps: str) -> int:
+def three_cups2(swaps: str) -> int:
     ball_location = 1
 
     for swap in swaps:
@@ -20,6 +20,18 @@ def three_cups(swaps: str) -> int:
                 ball_location = 3
             elif ball_location == 3:
                 ball_location = 1
+
+    return ball_location
+
+
+def three_cups(swaps: str) -> int:
+    ball_location = 1
+
+    swaps_paths = {("A", 1): 2, ("A", 2): 1, ("B", 2): 3, ("B", 3): 2, ("C", 1): 3, ("C", 3): 1}
+
+    for swap in swaps:
+        if (new_location := swaps_paths.get((swap, ball_location))) is not None:
+            ball_location = new_location
 
     return ball_location
 
